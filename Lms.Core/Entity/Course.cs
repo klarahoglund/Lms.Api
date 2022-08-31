e3da694a3d9dae010a7e4bd98e9305f190dae459
Klara Höglund <klarahoglund@hotmail.com>
2022-08-31 11:10:23 +0200

    Add project files.

---
+++ Lms.Core/Entity/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace Lms.Core.Entity
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+
+        public ICollection<Module> Modules  { get; set; } = new List<Module>();
+    }
+}
---
+++ Lms.Core/Entity/Module.cs
@@ -0,0 +1,14 @@
+﻿namespace Lms.Core.Entity
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public int ? CourseId;
+
+        public Course Course { get; set; }
+    }
+}
